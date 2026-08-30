6a79296dbbd78e5d16e5b1e605e782bdc04d7099
tatagamers26-web <tatagamers26@gmail.com>
2026-08-30 12:19:54 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -126,12 +126,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-  <script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5036627158328757"
-    crossOrigin="anonymous"
-  />
+      <head
 
   <script
     type="application/ld+json"
