fa1cb0ce82e98cf809215c007d3feae9de56d003
tatagamers26-web <tatagamers26@gmail.com>
2026-08-30 12:45:32 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -136,6 +136,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 
   gtag('config', 'G-C94G39B2G3');
 </script>
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBW4GMPD');</script>
+<!-- End Google Tag Manager -->
   <script
     type="application/ld+json"
     dangerouslySetInnerHTML={{
